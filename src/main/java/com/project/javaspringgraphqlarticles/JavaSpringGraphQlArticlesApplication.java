11a25e8f3f27a0dc52707c3981553778f985602d
Leonardo <leosimoes.engcomp@gmail.com>
2024-04-07 14:57:16 -0300

    Project started

---
+++ src/main/java/com/project/javaspringgraphqlarticles/JavaSpringGraphQlArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.project.javaspringgraphqlarticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringGraphQlArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringGraphQlArticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/javaspringgraphqlarticles/JavaSpringGraphQlArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.javaspringgraphqlarticles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringGraphQlArticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
